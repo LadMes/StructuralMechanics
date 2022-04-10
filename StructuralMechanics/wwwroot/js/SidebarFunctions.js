5c2ea490568f96915888dd969fc29dcb4672a142
LadMes <dartx7x@gmail.com>
2022-04-10 23:24:12 +0400

    Move the js code to its own file. Add links to the sidebar. Solve issue #1.

---
+++ StructuralMechanics/wwwroot/js/SidebarFunctions.js
@@ -0,0 +1,15 @@
+﻿function showHiddenMenu(menuId, btnId) {
+    var hiddenMenu = document.getElementById(menuId);
+    var liMenu = document.getElementById(btnId);
+    var btnMenu = liMenu.firstElementChild;
+    if (hiddenMenu.style.display == "none") {
+        hiddenMenu.style.display = "flex";
+        liMenu.style.backgroundColor = "var(--color-header-background)";
+        btnMenu.style.color = "white";
+    }
+    else {
+        hiddenMenu.style.display = "none";
+        liMenu.style.background = "none";
+        btnMenu.removeAttribute("style");
+    }
+}
