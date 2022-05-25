a2a67dfcd5be654c2751a97580fae6e628daa49e
LadMes <dartx7x@gmail.com>
2022-05-25 21:36:38 +0400

    Add partial views for the Moments and ShearForces controllers.

--- StructuralMechanics/Areas/Project/ViewModels/MomentViewModel.cs
+++ StructuralMechanics/Areas/Project/ViewModels/MomentViewModel.cs
@@ -2,16 +2,11 @@
 
 namespace StructuralMechanics.Areas.Project.ViewModels
 {
-    public class ShearForceViewModel : PointsViewModel
+    public class MomentViewModel : BaseViewModel
     {
         [Required]
         public double Magnitude { get; set; }
         [Required]
         public Direction Direction { get; set; }
-
-        [Required]
-        [Display(Name = "Location")]
-        public int LocationId { get; set; }
-        public Point? Location { get; set; }
     }
 }
